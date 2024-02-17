a847e1a53e58e4d4566d3494c9c4de1815c05887
yungshun317 <seraph317@gmail.com>
2024-02-17 18:00:38 +0800

    [FEAT] Create a `store` instance which uses the reducer function to generate the initial state and to calculate any future updates

--- react-redux-video-chat/src/App.js
+++ react-redux-video-chat/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import { useEffect } from "react";
+import Dashboard from "./Dashboard/Dashboard";
+import LoginPage from "./LoginPage/LoginPage";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/">
+          <LoginPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ react-redux-video-chat/src/store/reducers/dashboardReducer.js
@@ -0,0 +1,19 @@
+import * as dashboardActions from '../actions/dashboardActions';
+
+const initState = {
+    username: ''
+};
+
+const reducer = (state = initState, action) => {
+        switch (action.type) {
+            case dashboardActions.DASHBOARD_SET_USERNAME:
+                return {
+                    ...state,
+                    username: action.username
+                };
+            default:
+                return state;
+        }
+    };
+
+export default reducer;
