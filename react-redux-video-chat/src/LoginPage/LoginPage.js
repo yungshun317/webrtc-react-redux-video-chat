f912c6687ac27a28e366f4b23e2f55689f36029c
yungshun317 <seraph317@gmail.com>
2024-05-22 18:21:58 +0800

    [FEAT] Dispatch actions with `mapDispatchToProps`

--- react-redux-video-chat/src/LoginPage/LoginPage.js
+++ react-redux-video-chat/src/LoginPage/LoginPage.js
@@ -1,8 +1,42 @@
-const LoginPage = () => {
+import SubmitButton from "./components/SubmitButton";
+import {useState} from "react";
+import UsernameInput from "./components/UsernameInput";
+import {setUsername} from "../store/actions/dashboardActions";
+import {connect} from "react-redux";
+import { useNavigate } from 'react-router-dom';
+import logo from '../resources/logo.png';
+import './LoginPage.css';
+
+const LoginPage = ({ saveUsername }) => {
+    const [username, setUsername] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmitButtonPressed = () => {
+        navigate('/dashboard');
+        saveUsername(username);
+    };
+
     return (
-        <>
-        </>
+        <div className='login-page_container background_main_color'>
+            <div className='login-page_login_box background_secondary_color'>
+                <div className='login-page_logo_container'>
+                    <img className='login-page_logo_image' src={logo} alt='VideoTalker' />
+                </div>
+                <div className='login-page_title_container'>
+                    <h2>Get on Board</h2>
+                </div>
+                <UsernameInput username={username} setUsername={setUsername}/>
+                <SubmitButton handleSubmitButtonPressed={handleSubmitButtonPressed}/>
+            </div>
+        </div>
     );
 };
 
-export default LoginPage;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        saveUsername: username => dispatch(setUsername(username))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(LoginPage);
