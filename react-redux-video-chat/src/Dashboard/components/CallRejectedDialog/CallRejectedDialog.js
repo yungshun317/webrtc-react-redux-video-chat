8c36f6d9085f23fcb2a97d12f4ffb6e6e204f46b
yungshun317 <seraph317@gmail.com>
2024-06-07 22:15:46 +0800

    [FEAT] Create dialogs related with direct calls

---
+++ react-redux-video-chat/src/Dashboard/components/CallRejectedDialog/CallRejectedDialog.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './CallRejectedDialog.css';
+
+const CallRejectedDialog = () => {
+    return (
+        <div className='call_rejected_dialog background_secondary_color'>
+      <span>
+        Call rejected
+      </span>
+        </div>
+    );
+};
+
+export default CallRejectedDialog;
